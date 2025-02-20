df117f1b5aae772740b32a9b767408c150f30e89
namgyeongtae <gotsuebel@gmail.com>
2025-02-20 23:09:40 +0900

    Add Data Model Class

---
+++ Assets/Scripts/Data/MusicData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicData
+{
+    public string title;
+    public string artist;
+    public string album;
+    public string clip;
+    public string video;
+    public float showtime;
+}
