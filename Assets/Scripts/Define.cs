fcd61edd4a0ca650019b1ba7ca934d7f828e41e2
namgyeongtae <gotsuebel@gmail.com>
2025-03-03 15:34:17 +0900

    Add const value of note speed

--- Assets/Scripts/Define.cs
+++ Assets/Scripts/Define.cs
@@ -7,4 +7,7 @@ public class Define : MonoBehaviour
     public const float NOTE_REMOVE_Y = 100;
 
     public const float MULTIPLIER_SEC_TO_MS = 1000f;
+
+    public const float NOTE_SPEED_MIN = 1000f;
+    public const float NOTE_SPEED_MAX = 4000f;
 }
